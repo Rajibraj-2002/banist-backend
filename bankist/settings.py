963829ef1e386ea6e860715e2da77feac0e69fe6
Rajibraj-2002 <rajibrajraymohapatra@gmail.com>
2025-07-21 19:51:12 +0530

    Fix: added render domain to ALLOWED_HOSTS

--- bankist/settings.py
+++ bankist/settings.py
@@ -178,3 +178,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['banist-backend.onrender.com', 'localhost', '127.0.0.1']
