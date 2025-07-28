914b14e7e49ace7b3a77a0620f328800ad1faa22
Rajibraj-2002 <rajibrajraymohapatra@gmail.com>
2025-07-28 21:03:45 +0530

    Connected PostgreSQL to Django

--- bankist/settings.py
+++ bankist/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,11 +93,19 @@ WSGI_APPLICATION = 'bankist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://bankist_db_user:nNtWEmlcNDuKu3TFPLBqrO2m3xXJ5prn@dpg-d23paafdiees739t1sa0-a/bankist_db'),
+        conn_max_age=600,
+        ssl_require=True  # Required for Render PostgreSQL
+    )
 }
 
 # Email for OTP
